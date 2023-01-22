1d27fa88be7b219096f0ef075f27548c44969c30
kobipro <Boatengkwabenaakuamoah@gmail.com>
2023-01-22 03:32:58 +0000

    added the first phase of the navbar section

--- src/Components/Navbar.component/navbar.component.jsx
+++ src/Components/Navbar.component/navbar.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./navbar.component.css"
 
 function Navbar() {
   return (
-    <div>This is the navbar component</div>
+    <div className='Nav_bar Nav_black' >
+      <div className='Nav_content'>
+        <img className='Nav_logo' src='https://pngimg.com/uploads/netflix/netflix_PNG25.png' alt='netflix-logo'/>
+        <img className='Nav_avatar' src='https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg' alt='netflix-avatar'/>
+      </div>
+      
+    </div>
   )
 }
 
