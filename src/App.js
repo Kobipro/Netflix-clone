7fe6d717b8ea45e5e2e457d0a8b5b641834b9ce0
kobipro <Boatengkwabenaakuamoah@gmail.com>
2023-01-22 01:51:58 +0000

    added the skeleton of the netflix clone project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>Hello Kobby FAM!</div>
+        <div>This is my netflix clone!</div>
+        {/* <Counter /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXQugJAp8hcedJmbf0ySsACiMxpXrYcPs",
+  authDomain: "netflix-clone-d95ea.firebaseapp.com",
+  projectId: "netflix-clone-d95ea",
+  storageBucket: "netflix-clone-d95ea.appspot.com",
+  messagingSenderId: "50493617258",
+  appId: "1:50493617258:web:43a5c9cfa4605621d5c613"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
