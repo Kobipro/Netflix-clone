2009c1531b99efc8f4f107f519d5fc0134863c57
kobipro <Boatengkwabenaakuamoah@gmail.com>
2023-01-24 20:58:26 +0000

    added axios,request js and the truncate function

--- src/request.js
+++ src/request.js
@@ -1,8 +1,12 @@
-const API_key = "" ;
+const API_KEY = "5cc615e73bb1d16941d8c67ddd8421ac" ;
 const requests ={
-// copy 
-// it
-// from 
-// sagna
+    fetchTrending: `./trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`./discover/tv?api_keys=${API_KEY}&with_networks=213`,
+    fetchTopRated :`./discover/movie?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`./movie/top_rated?/api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`./discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`./discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`./discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`./discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 export default requests;
